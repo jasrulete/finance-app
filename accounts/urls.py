2b321a0a41d515b1fc534bf245ef12772f4c2e4a
gelo <angelotorres200425@gmail.com>
2025-05-03 12:57:17 +0800

    login default url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
